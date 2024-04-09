976f66078eb38546391412a6f5ca9ff14ce1dab7
Денисова Алиса Евгеньевна <adeavoice@gmail.com>
2024-04-09 20:53:48 +0300

    Adding communication between windows

--- main.py
+++ main.py
@@ -16,12 +16,57 @@ class Registration(QtWidgets.QMainWindow):
         else:
             self.ui.error.setText("Не введены данные для входа")
 
-
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        uic.loadUi('main_window.ui', self)
+        self.ui = uic.loadUi('main_window.ui', self)
+        self.ui.settings_btn.clicked.connect(self.show_settings_window)
+        self.ui.search_btn.clicked.connect(self.show_search_window)
 
+    def show_settings_window(self):
+        self.settings_window = SettingsWindow()
+        self.settings_window.show()
+        self.close()
+
+    def show_search_window(self):
+        self.search_window = SearchWindow()
+        self.search_window.show()
+        self.close()
+
+class SettingsWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(SettingsWindow, self).__init__()
+        self.ui = uic.loadUi('settings.ui', self)
+        self.ui.exit_btn.clicked.connect(self.register)
+        self.ui.return_btn.clicked.connect(self.show_main_window)
+    
+    def register(self):
+        self.registration = Registration()
+        self.registration.show()
+        self.close()
+    
+    def show_main_window(self):
+        self.search_window = MainWindow()
+        self.search_window.show()
+        self.close()
+
+class SearchWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(SearchWindow, self).__init__()
+        self.ui = uic.loadUi('search_window.ui', self)
+        self.ui.return_btn.clicked.connect(self.show_main_window)
+        self.ui.settings_btn.clicked.connect(self.show_settings_window)
+
+    def show_settings_window(self):
+        self.settings_window = SettingsWindow()
+        self.settings_window.show()
+        self.close()
+
+    def show_main_window(self):
+        self.search_window = MainWindow()
+        self.search_window.show()
+        self.close()
+    
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
